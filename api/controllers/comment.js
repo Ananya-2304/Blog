76b3da7969fa4dcfb69aa119661a26bbe326007f
Ananya-2304 <92137448+Ananya-2304@users.noreply.github.com>
2024-09-02 08:34:44 +0530

    Update comment.js

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -7,8 +7,8 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        user: 'ananyabhat23@gmail.com',
-        pass: 'rjtdmgeggondfxdr'
+        user: 'example@gmail.com',
+        pass: 'password'
     }
 });
 function checkEmailQueue() {
